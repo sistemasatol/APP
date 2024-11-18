4e3db07216af27f4c39cd0899942d0b7b3447ac6
sistemasatol <sistemas@atolincorporadora.com.br>
2024-11-18 16:33:00 -0300

    Configurar Servidor do Vite

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    host: '0.0.0.0',  // Aceita conexões externas
+    port: 5173,       // Porta opcional, se necessário
+  },
+
   plugins: [react()],
 })
