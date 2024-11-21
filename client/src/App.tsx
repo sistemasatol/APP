adb7abc401fcd91a2d419bb709290de8b3177c67
sistemasatol <sistemas@atolincorporadora.com.br>
2024-11-21 08:37:19 -0300

    Instalar react router dom e implementar rotas

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,19 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import Employees from './pages/Employees'
-// import Enterprises from './pages/Enterprises'
+import Enterprises from './pages/Enterprises'
 function App() {
 
   return (
     <div>
-      <Employees />
-      {/* <Enterprises /> */}
+
+      <BrowserRouter>
+        <Routes>
+          <Route element={<Employees />} path="/funcionarios" />
+          <Route element={<Enterprises />} path="/empresas" />
+        </Routes>
+      </BrowserRouter>
+
     </div>
   )
 }
